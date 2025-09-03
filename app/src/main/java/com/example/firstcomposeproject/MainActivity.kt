11797fdee4c4b8ad2f0d9576e441f6ace880e027
Pavel Potapov <potapov@goozix.com>
2025-09-03 21:39:02 +0300

    Add lesson 2.2

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,35 +12,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    UserInfo("Pavel", 30)
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                UserInfo(
+                    "Pavel",
+                    30,
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UserInfoPreview() {
+    UserInfo("Pavel", 30)
 }
 
 @Composable
-fun UserInfo(name: String, age: Int) {
-    Text(
-        text = "Hello $name! You are $age years old"
-    )
+fun UserInfo(name: String, age: Int, modifier: Modifier = Modifier) {
+    Column {
+        repeat(10) {
+            Text(
+                text = "Hello $name! You are $age years old",
+                modifier = modifier
+            )
+        }
+    }
 }
